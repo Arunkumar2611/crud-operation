9774501bfea9d66b9a87a321c7e99dcf817d1256
Arunkumar2611 <arunkumar2611@github.com>
2022-01-14 18:56:09 +0530

    adding route for get req for 1 user by passing that user id

--- routes/users.js
+++ routes/users.js
@@ -2,18 +2,7 @@ import express from 'express';
 import { v4 as uuidv4 } from 'uuid';
 const router = express.Router();
 
-const users = [
-    {
-        firstName: "John",
-        lastname: "Doe",
-        age: 25
-    },
-    {
-        firstName: "jan",
-        lastname: "Doe",
-        age: 24
-    }
-]
+const users = []
 
 router.get('/', (req, res) => {
     console.log(users);
@@ -26,8 +15,13 @@ router.post('/', (req, res) => {
     const userWithId = { ...user, id: userId }
     users.push(userWithId);
     res.send(`user with name ${user.firstName} added to the database`);
-})
+});
+
+router.get('/:id', (req, res) => {
+    // console.log(req.params);
+    const { id } = req.params;
+    const foundUser  = users.find((user) => user.id === id);
+    res.send(foundUser);
+});
 
 export default router;
-
-// here we have install npm package UUID that gives a unique string id

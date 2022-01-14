2a35d0924c0e1a1b47647f519bc07aab460b059d
Arunkumar2611 <arunkumar2611@github.com>
2022-01-14 18:16:19 +0530

    adding raw data and check the get request response

--- routes/users.js
+++ routes/users.js
@@ -2,18 +2,22 @@ import express from 'express';
 
 const router = express.Router();
 
-// all routes in here are starting with /users
-router.get('/userroute', (req, res) => {
-    res.send('Hello');
+const users = [
+    {
+        firstName: "John",
+        lastname: "Doe",
+        age: 25
+    },
+    {
+        firstName: "jan",
+        lastname: "Doe",
+        age: 24
+    }
+]
+
+router.get('/', (req, res) => {
+    console.log(users);
+    res.send(users);
 });
-
-/*
-if we write route like this
-
-            router.get('/user', (req, res) => {
-                res.send('Hello');
-            });
-then we need to write in the brower to call this get method like this: localhost:5000/users/userroute
-*/
 
 export default router;
